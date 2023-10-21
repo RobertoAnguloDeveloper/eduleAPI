f53cd6ab4acbff7c1d264f1d402d6ba412985708
RobertoAnguloDeveloper <rangulot@outlook.com>
2023-10-20 22:36:18 -0500

    Spring Security issue

--- src/main/java/com/cipasoft/edule/models/Subject.java
+++ src/main/java/com/cipasoft/edule/models/Subject.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -24,11 +26,14 @@ public class Subject {
     private String description;
 
     @OneToMany(mappedBy = "subject")
+    @JsonIgnoreProperties("subject")
     private List<Hour> hours;
 
     @OneToMany(mappedBy = "subject")
+    @JsonIgnoreProperties("subject")
     private List<Topic> classes;
 
     @OneToMany(mappedBy = "subject")
+    @JsonIgnoreProperties("subject")
     private List<UserSubject> userSubjects;
 }
