c843a9ed42d2a6c0b23397ccebdd17617692ae75
RobertoAnguloDeveloper <rangulot@outlook.com>
2023-11-16 19:16:39 -0500

    Changes?

--- src/main/java/com/cipasoft/edule/controllers/ConCycle.java
+++ src/main/java/com/cipasoft/edule/controllers/ConCycle.java
@@ -14,50 +14,28 @@ import java.util.List;
 @CrossOrigin(origins = "*")
 public class ConCycle {
     @Autowired
-<<<<<<< HEAD
     private ServCycle CycleService;
 
     @PostMapping("/save")
     @ResponseStatus(HttpStatus.CREATED)
     public void createCycle(@RequestBody Cycle Cycle) {
         CycleService.createCycle(Cycle);
-=======
-    private ServCycle cycleService;
-
-    @PostMapping("/save")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createCycle(@RequestBody Cycle cycle) {
-        cycleService.createCycle(cycle);
->>>>>>> 9f2752f2fba057da02051bd38d798856e255e993
     }
 
     @GetMapping("/all")
     public List<Cycle> getAllCycles() {
-<<<<<<< HEAD
         return CycleService.getAllCycles();
-=======
-        return cycleService.getAllCycles();
->>>>>>> 9f2752f2fba057da02051bd38d798856e255e993
     }
 
     @PutMapping("/update")
     @ResponseStatus(HttpStatus.CREATED)
-<<<<<<< HEAD
     public Cycle updateCycle(@RequestBody Cycle Cycle) {
         return CycleService.updateCycle(Cycle);
-=======
-    public Cycle updateCycle(@RequestBody Cycle cycle) {
-        return cycleService.updateCycle(cycle);
->>>>>>> 9f2752f2fba057da02051bd38d798856e255e993
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCycle(@PathVariable("id") Integer id) {
-<<<<<<< HEAD
         CycleService.deleteCycle(id);
-=======
-        cycleService.deleteCycle(id);
->>>>>>> 9f2752f2fba057da02051bd38d798856e255e993
     }
 }
--- src/main/java/com/cipasoft/edule/services/ServCycle.java
+++ src/main/java/com/cipasoft/edule/services/ServCycle.java
@@ -12,7 +12,6 @@ import java.util.Optional;
 @Service
 public class ServCycle {
     @Autowired
-<<<<<<< HEAD
     private RepoCycle CycleRepository;
 
     public List<Cycle> getAllCycles() {
@@ -33,45 +32,5 @@ public class ServCycle {
 
     public void deleteCycle(Integer id) {
         CycleRepository.deleteById(id);
-=======
-    private RepoCycle RepoCycle;
-
-    public List<Cycle> getAllCycles() {
-        return RepoCycle.findAll();
-    }
-
-    public Optional<Cycle> getCycleById(Integer id) {
-        return RepoCycle.findById(id);
-    }
-
-    public Cycle createCycle(Cycle cycle) {
-        return RepoCycle.save(cycle);
-    }
-
-    public Cycle updateCycle(Cycle cycle) {
-        if (cycle != null && cycle.getId() != null) {
-            if (RepoCycle.existsById(cycle.getId())) {
-                Optional<Cycle> oldCycle = RepoCycle.findById(cycle.getId());
-                Cycle updatedCycle = oldCycle.get();
-
-                if (cycle.getCycle_number() != null) {
-                    updatedCycle.setCycle_number(cycle.getCycle_number());
-                }
-                if (cycle.getDay() != null) {
-                    updatedCycle.setDay(cycle.getDay());
-                }
-
-                return RepoCycle.save(updatedCycle);
-            } else {
-                return null;
-            }
-        } else {
-            return null;
-        }
-    }
-
-    public void deleteCycle(Integer id) {
-        RepoCycle.deleteById(id);
->>>>>>> 9f2752f2fba057da02051bd38d798856e255e993
     }
 }
